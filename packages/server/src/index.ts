177841c9fe891c277039f5f4416c8b9300dbde7e
scobru <scobru1988@gmail.com>
2024-07-01 10:39:40 +0200

    add server scripts

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,5 +2,7 @@ import "./fetchUnitPrices";
 import "./fetchTotalValuation";
 import "./fetchInterestEarned";
 import "./dcaExecutor";
-import "./deleteOldRecords"
+import "./deleteOldRecords";
+import "./reinvestEarnings";
+import "./rebalancePools";
 import "./server";
