bff48ab00c037d914159c3155f4868447fee68c2
scobru <scobru1988@gmail.com>
2024-07-03 15:58:20 +0200

    updates server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: "http://localhost:3001", // Replace with your frontend's domain
+    origin: "*", // Replace with your frontend's domain
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type,Authorization",
   }),
